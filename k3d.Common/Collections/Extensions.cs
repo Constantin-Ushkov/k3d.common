f825c5f2bbb68cf0e0a4e07c35ef7aa4c91eb3d5
Constantin Ushkov <konst3d@gmail.com>
2024-12-24 11:20:49 +0200

    Added DateTime extensions.

---
+++ k3d.Common/Collections/Extensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace k3d.Common.Collections
+{
+    public static class Extensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                collection.Add(item);
+            }
+        }
+    }
+}
---
+++ k3d.Common/DateTime/Extensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace k3d.Common.DateTime
+{
+    public static class Extensions
+    {
+        public static System.DateTime TrimMilliseconds(this System.DateTime dt)
+        {
+            return dt.AddMilliseconds(-dt.Millisecond);
+        }
+    }
+}
